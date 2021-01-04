b83d0923c23e3a91b4db67183866c4e3c52244d7
Jürgen Untch <j.untch@outlook.de>
2021-01-04 21:58:29 +0100

    add fade-in-menu project

---
+++ fade-in-menu/script.js
@@ -0,0 +1,21 @@
+/*
+
+// This code works but is not neccessary to make the menu fade in
+// the fade-in-effect was implemented via :target selector
+// further info: https://css-tricks.com/off-canvas-menu-with-css-target/
+
+
+
+const navOpenBtn = document.querySelector('.nav-btn.open-btn');
+const navCloseBtn = document.querySelector('.nav-btn.close-btn');
+const nav = document.querySelector('.nav');
+
+navOpenBtn.addEventListener('click', () => {
+  nav.classList.toggle('open');
+});
+
+navCloseBtn.addEventListener('click', () => {
+  nav.classList.toggle('open');
+});
+
+*/
